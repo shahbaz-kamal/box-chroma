22b306f92a8b6a6b109a2fadfd9dd614e2befa7f
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-04-04 19:01:34 +0600

    changed title color

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,13 @@ function App() {
 
   return (
     <div className="container mx-auto ">
-      <h1 className="text-center my-5 md:my-6  text-3xl md:text-4xl font-bold">
-        Color Changing Box
-      </h1>
+      <div className="flex justify-center">
+        {" "}
+        <h1 className="text-center inline-flex my-5 md:my-6  text-3xl md:text-4xl font-bold bg-gradient-to-r from-green-500 p-6 to-orange-500 bg-clip-text text-transparent">
+          Color Changing Box
+        </h1>
+      </div>
+
       <section>
         <Grid></Grid>
       </section>
