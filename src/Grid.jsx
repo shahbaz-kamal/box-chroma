ec96269ad60ee1ac122dc213b810615457575ef7
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-04-04 18:14:37 +0600

    added functionality while clicking the box

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 
 const Grid = () => {
-  const size = 3; // 3x3 grid
-  const [grid, setGrid] = useState(Array(size * size).fill());
+  const [defaultArray, setDefaultArray] = useState(Array(9).fill("white"));
   const [order, setOrder] = useState([]);
-  console.log(grid);
+  console.log(defaultArray);
+
+  const handleClick = (index) => {
+    if (!order.includes(index)) {
+      const newArray = [...defaultArray];
+      newArray[index] = "green";
+      setDefaultArray(newArray);
+
+      setOrder([...order, index]);
+    }
+  };
   return (
-    <div className="grid grid-cols-3 "> 
-      {grid.map((box, index) => (
-        <div key={index} className="border-2 border-gray-200 w-full h-[200px]"></div>
+    <div className="grid grid-cols-3 ">
+      {defaultArray.map((backgroundColor, index) => (
+        <div
+          onClick={() => handleClick(index)}
+          style={{ backgroundColor: backgroundColor }}
+          key={index}
+          className="border-2 border-gray-200 w-full h-[200px] flex items-center justify-center"
+        >
+          <p className="text-white text-2xl font-bold">
+            {order.includes(index) ? order.indexOf(index) + 1 : ""}
+          </p>
+        </div>
       ))}
     </div>
   );
