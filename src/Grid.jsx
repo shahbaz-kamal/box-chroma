f154a58aede7e0fe6ee426576d9a8aaa7be9b4a2
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-04-04 18:29:03 +0600

    project completed

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 const Grid = () => {
   const [defaultArray, setDefaultArray] = useState(Array(9).fill("white"));
   const [order, setOrder] = useState([]);
-  console.log(defaultArray);
 
   const handleClick = (index) => {
     if (!order.includes(index)) {
@@ -12,7 +11,22 @@ const Grid = () => {
       setDefaultArray(newArray);
 
       setOrder([...order, index]);
+      if (order.length + 1 === 9) {
+        orangeSequence([...order, index]);
+      }
     }
+  };
+
+  const orangeSequence = (order) => {
+    order.forEach((idx, idxForThisLoop) => {
+      setTimeout(() => {
+        setDefaultArray((prev) => {
+          const newArray = [...prev];
+          newArray[idx] = "orange";
+          return newArray;
+        });
+      }, idxForThisLoop * 1000);
+    });
   };
   return (
     <div className="grid grid-cols-3 ">
@@ -21,7 +35,7 @@ const Grid = () => {
           onClick={() => handleClick(index)}
           style={{ backgroundColor: backgroundColor }}
           key={index}
-          className="border-2 border-gray-200 w-full h-[200px] flex items-center justify-center"
+          className="border-2 border-gray-200 w-full h-[200px] flex items-center justify-center cursor-pointer"
         >
           <p className="text-white text-2xl font-bold">
             {order.includes(index) ? order.indexOf(index) + 1 : ""}
